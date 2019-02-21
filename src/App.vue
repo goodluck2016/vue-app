3133780d357174b95a4a1e993972fc88e8f5d78a
goodluck2016 <zhangyumei_2019@163.com>
2019-02-21 14:00:50 +0800

    App

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,20 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
+    <NavBottom />
   </div>
 </template>
 
 <script>
+import NavBottom from './components/public/navBottom'
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+    }
+  },
+  components: {
+    NavBottom
+  }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
