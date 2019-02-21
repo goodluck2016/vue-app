955bd2edb8870fd720dcf05beeddc13f1ee3e880
goodluck2016 <zhangyumei_2019@163.com>
2019-02-21 14:01:53 +0800

    router

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,47 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import home from '@/components/home'
+import product from '@/components/product'
+import agent from '@/components/agent'
+import cases from '@/components/cases'
+import about from '@/components/about'
 
 Vue.use(Router)
-
 export default new Router({
+  linkActiveClass: 'active',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      redirect: '/home'
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: home
+    },
+    {
+      path: '/product',
+      name: 'product',
+      component: product
+    },
+    {
+      path: '/agent',
+      name: 'agent',
+      component: agent
+    },
+    {
+      path: '/cases',
+      name: 'cases',
+      component: cases
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: about
+    },
+    {
+      path: '*',
+      component: home
     }
   ]
 })
