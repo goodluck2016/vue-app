3aa6131dcbdd3aba6c62d817189bb9666de37a86
goodluck2016 <zhangyumei_2019@163.com>
2019-02-21 14:01:18 +0800

    main.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import './assets/font/iconfont.css'
+import './assets/css/base.css'
 
 Vue.config.productionTip = false
 
