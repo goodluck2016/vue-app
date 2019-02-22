8ee5ff2e2d868609b8d5be5e4d58fd2ad9262a48
goodluck2016 <zhangyumei_2019@163.com>
2019-02-22 17:33:42 +0800

    public

---
+++ src/components/public/topBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="topBar">
+    <a onclick="window.history.go(-1)" class="icon_back"></a>
+    <h3 class="cartname" v-title :data-title="titleName">{{titleName}}</h3>
+    <a href="#" class="icon_menu" @click="goHome($event)"><i class="iconfont">&#xe60d;</i></a>
+  </div>
+</template>
+<script>
+import title from '../../directive/title'
+export default {
+  data () {
+    return {
+    }
+  },
+  props: ['titleName'],
+  mounted: function () {
+  },
+  methods: {
+  },
+  directives: {
+    title
+  }
+}
+</script>
+<style scoped>
+</style>
