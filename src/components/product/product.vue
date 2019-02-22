06014b2cfb8052fcc7040b49ee4cf6f5509a2210
goodluck2016 <zhangyumei_2019@163.com>
2019-02-22 17:31:11 +0800

    product

---
+++ src/components/product/product.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="product">
+    <topBar :titleName='titleName'/>
+    <div class="main">
+      <banner :imgDatas='imgDatas' />
+    </div>
+  </div>
+</template>
+<script>
+import topBar from '../public/topBar'
+import banner from '../public/banner'
+export default {
+  titleName: 'product',
+  data () {
+    return {
+      titleName: '产品',
+      imgDatas: [require('../../assets/images/banner1.png'), require('../../assets/images/banner2.png')]
+    }
+  },
+  components: {
+    topBar,
+    banner
+  },
+  methods: {
+    goHome (event) {
+      this.$router.push('/home')
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
