e8247c5580d0de144dcace0a858103da783ee6e3
goodluck2016 <zhangyumei_2019@163.com>
2019-02-23 16:32:45 +0800

    cart

---
+++ src/components/cart/cart.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="cart">
+    <topBar :titleName='titleName'/>
+    <div class="main"></div>
+  </div>
+</template>
+<script>
+import topBar from '../public/topBar'
+export default {
+  name: 'cart',
+  data () {
+    return {
+      titleName: '购物车'
+    }
+  },
+  mounted () {
+    this.$store.dispatch('hideNav')
+  },
+  destroyed () {
+    this.$store.dispatch('showNav')
+  },
+  methods: {
+  },
+  components: {
+    topBar
+  }
+}
+</script>
+<style scoped>
+</style>
