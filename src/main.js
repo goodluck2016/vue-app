0cca9e2a1a53303649024fbba138ebd04a04195b
goodluck2016 <zhangyumei_2019@163.com>
2019-02-25 20:32:09 +0800

    router

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-// import axios from 'axios'
+import axios from 'axios'
 import App from './App'
 import router from './router'
 import store from './store/index.js'
@@ -9,9 +9,9 @@ import './assets/font/iconfont.css'
 import './assets/css/base.css'
 
 Vue.config.productionTip = false
-// axios.defaults.baseURL = 'http://192.168.2.115:3333/'
-// axios.defaults.headers['Content-Type'] = 'application/x-www-form-urlencoded'
-// Vue.prototype.$http = axios
+axios.defaults.baseURL = 'http://localhost:3333/'
+axios.defaults.headers['Content-Type'] = 'application/x-www-form-urlencoded'
+Vue.prototype.$http = axios
 
 /* eslint-disable no-new */
 new Vue({
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import register from '@/components/login/register'
+import login from '@/components/login/login'
 import home from '@/components/home/home'
+import category from '@/components/category/category'
+import search from '@/components/search/search'
 import product from '@/components/product/product'
+import detail from '@/components/detail/detail'
+import cart from '@/components/cart/cart'
 import agent from '@/components/agent/agent'
 import cases from '@/components/cases/cases'
 import about from '@/components/about/about'
@@ -15,14 +21,44 @@ export default new Router({
       redirect: '/home'
     },
     {
+      path: '/register',
+      name: 'register',
+      component: register
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: login
+    },
+    {
       path: '/home',
       name: 'home',
       component: home
     },
     {
+      path: '/category',
+      name: 'category',
+      component: category
+    },
+    {
+      path: '/search',
+      name: 'search',
+      component: search
+    },
+    {
       path: '/product',
       name: 'product',
       component: product
+    },
+    {
+      path: '/detail',
+      name: 'detail',
+      component: detail
+    },
+    {
+      path: '/cart',
+      name: 'cart',
+      component: cart
     },
     {
       path: '/agent',
