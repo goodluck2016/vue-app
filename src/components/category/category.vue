48a35835d88b551fa996821cb91b7d08cfc7927a
goodluck2016 <zhangyumei_2019@163.com>
2019-02-25 20:37:02 +0800

    search | detail | category

---
+++ src/components/category/category.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="cart">
+    <topBar :titleName='titleName'/>
+    <div class="main"></div>
+  </div>
+</template>
+<script>
+import topBar from '../public/topBar'
+export default {
+  name: 'category',
+  data () {
+    return {
+      titleName: '商品分类'
+    }
+  },
+  mounted () {
+    // this.$store.dispatch('hideNav')
+  },
+  destroyed () {
+    // this.$store.dispatch('showNav')
+  },
+  methods: {
+  },
+  components: {
+    topBar
+  }
+}
+</script>
+<style scoped>
+</style>
---
+++ src/components/detail/detail.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="cart">
+    <topBar :titleName='titleName'/>
+    <div class="main"></div>
+  </div>
+</template>
+<script>
+import topBar from '../public/topBar'
+export default {
+  name: 'detail',
+  data () {
+    return {
+      titleName: '商品详情'
+    }
+  },
+  mounted () {
+    // this.$store.dispatch('hideNav')
+  },
+  destroyed () {
+    // this.$store.dispatch('showNav')
+  },
+  methods: {
+  },
+  components: {
+    topBar
+  }
+}
+</script>
+<style scoped>
+</style>
