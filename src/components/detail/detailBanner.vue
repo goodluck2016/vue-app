b73ba3c88bb33adceb6d9c8d260b5f759d673c06
goodluck2016 <zhangyumei_2019@163.com>
2019-03-01 14:40:56 +0800

    detail

--- src/components/detail/detailBanner.vue
+++ src/components/detail/detailBanner.vue
@@ -35,44 +35,4 @@ export default {
 }
 </script>
 <style scoped>
-/*.banner {
-    width: 100%;
-    overflow: hidden;
-    position: relative;
-}
-.banner ul:first-child {
-    width: 1000%;
-}
-.banner ul:first-child li {
-    width: 10%;
-    float: left;
-}
-.banner ul:first-child li img {
-    width: 100%;
-    display: block;
-}
-.banner ul:last-child {
-    width: 20px;
-    height: 20px;
-    position: absolute;
-    left: 90%;
-    bottom: -2px;
-}
-.banner ul:last-child li {
-    color:#fff;
-    width: 6px;
-    height: 6px;
-    background-color: transparent;
-    border: 0;
-    border-radius: 0;
-    display:inline-block;
-    margin: 0 4px;
-    box-shadow: 0 0 5px rgba(0,0,0,.3);
-}
-.banner ul:last-child li:first-child {
-    margin-left: 0;
-}
-.banner ul:last-child li.active,.active{
-    color:#f60;
-}*/
 </style>
