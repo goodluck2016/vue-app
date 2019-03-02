14b01cc544f0825ac846ab28f94ada0f9f9c8a76
goodluck2016 <zhangyumei_2019@163.com>
2019-03-02 17:20:53 +0800

    login

--- adminPc/src/views/login/login.vue
+++ adminPc/src/views/login/login.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="login">
     <h1>{{ msg }}</h1>
+    <router-link class="loginBtn" to="/home">登录</router-link>
   </div>
 </template>
 
@@ -11,8 +12,28 @@ export default {
     return {
       msg: 'login'
     }
+  },
+  method: {
   }
 }
 </script>
 <style scoped>
+.login{
+  width:600px;
+  height:400px;
+  margin: 20px auto;
+  padding:20px;
+}
+.loginBtn{
+  display:block;
+  margin:20px auto;
+  width:120px;
+  height:30px;
+  line-height: 30px;
+  background: #f60;
+  color:#fff;
+  font-size:16px;
+  border-radius: 3px;
+  text-align: center;
+}
 </style>
