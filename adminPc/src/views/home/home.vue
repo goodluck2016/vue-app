c06a86f44443b2de73ffa2874f82fa5c732dfd1d
goodluck2016 <zhangyumei_2019@163.com>
2019-03-04 17:39:18 +0800

    home

--- adminPc/src/views/home/home.vue
+++ adminPc/src/views/home/home.vue
@@ -1,6 +1,33 @@
 <template>
-  <div>
-    <h1>{{ msg }}</h1>
+  <div class="wrap">
+    <div class="header">
+      <div class="logo"></div>
+      <div class="menu-wrap">
+        <ul>
+          <router-link to="/main/data" tag="li">数据统计</router-link>
+          <router-link to="/main/marketing" tag="li">营销</router-link>
+          <router-link to="/main/merchant" tag="li">商户</router-link>
+          <router-link to="/main/member" tag="li">会员</router-link>
+          <router-link to="/main/user" tag="li">用户</router-link>
+          <router-link to="/main/points" tag="li">积分</router-link>
+          <router-link to="/main/goods" tag="li">商品</router-link>
+          <router-link to="/main/order" tag="li">订单</router-link>
+          <router-link to="/main/marketing" tag="li">交易</router-link>
+          <router-link to="/main/manager" tag="li">管理员</router-link>
+          <router-link to="/main/setting" tag="li">商城设置</router-link>
+          <router-link to="/main/notice" tag="li">公告</router-link>
+        </ul>
+      </div>
+      <div class="header-info">
+        <router-link to="/login" class="login-out">退出</router-link>
+      </div>
+    </div>
+    <div class="main">
+      <router-view></router-view>
+    </div>
+    <div class="footer">
+      <p>Copyright © 2006 dd Inc. All Rights Reserved. 沪ICP备 06006666-1号</p>
+    </div>
   </div>
 </template>
 <script>
