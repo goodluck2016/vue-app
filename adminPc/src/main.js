07b74feaf60a8b42ab0e8e71d641a0e38f0397e1
goodluck2016 <zhangyumei_2019@163.com>
2019-03-05 15:31:27 +0800

    main.js

--- adminPc/src/main.js
+++ adminPc/src/main.js
@@ -2,9 +2,20 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+import VueRouter from 'vue-router'
+import Routers from './router/index'
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
+
+const RouterConfig = {
+  mode: 'hash',
+  // mode: 'history',
+  routes: Routers,
+  linkActiveClass: 'menu-actived'
+}
+
+const router = new VueRouter(RouterConfig)
 
 /* eslint-disable no-new */
 new Vue({
